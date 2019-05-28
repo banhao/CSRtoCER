502563d1c2ca368189226c85f71f9b9c39b2bbe5
banhao <banhao@gmail.com>
2019-05-28 11:09:14 -0600

    Update CSRtoCER.ps1
    Update to Version 1.01

--- CSRtoCER.ps1
+++ CSRtoCER.ps1
@@ -15,19 +15,19 @@
   <>
 
 .NOTES
-  Version:        1.0
+  Version:        1.01
   Author:         <HAO BAN/hao.ban@ehealthsask.ca>
-  Creation Date:  <05/23/2019>
-  Purpose/Change: Initial script development
-  
+  Creation Date:  <05/28/2019>
+  Purpose/Change: Fix the bug that list the CA Server based on Template Name is not exactly match.
+
 .EXAMPLE
   This PowerShell passed the test in PowerShell version 5.1
-  PS H:\>host  
+  PS H:\>host
 	Check the PowerShell version.
-  
+
   PS H:\>CSRtoCER.ps1 csrfilename
-	Output the Certificate Template and the other information which is in the CSR file. 
-	
+	Output the Certificate Template and the other information which is in the CSR file.
+
   PS H:\>CSRtoCER.ps1 csrfilename -cert
 	Generate the Certificate by using the CSR file.
 
@@ -53,14 +53,14 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 		$ForestLength = (((get-addomain | findstr "^Forest" |  foreach{ $_.ToString() } | %{ $_.Split(':')[1]; }).Trim()).Split('.')).count
 		$Delta = $DistinguishedNameLength-$ForestLength
 		$ForestDC = ((get-addomain | findstr "DistinguishedName" |  foreach{ $_.ToString() } | %{ $_.Split(':')[1]; }).Trim()).Split(',')[$Delta..$($DistinguishedNameLength-1)] -join ","
-				
+
 		Write-Output "CSR file $CSRfile Subject = "$CSRfileSubject
 		Write-Output ""
 		Write-Output "CSR file $CSRfile Key Length = $($CSRfileKeyLength)"
 		Write-Output ""
 		Write-Output "CSR file $CSRfile SANs = "$CSRfileSAN
 		Write-Output ""
-		
+
 		#-------------------------------------------------------------------------------------------------------------------------------------------------------
 		#Generate all the Certificate Templates Name List from every CA Server export into TemplatePropCommonName.csv.
 		#certutil.exe -Template | findstr "TemplatePropCommonName" | %{ $_.Split('=')[1]; } |  %{$_.Substring(1)} > .\TemplatePropCommonName.csv
@@ -80,10 +80,10 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 		foreach($line in Get-Content .\TemplatePropCommonName.csv) {
 			$TemplateName = $line | %{ $_.Split(',')[1]; }
 			$Parameter = "CN="+$TemplateName+",CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,"+$ForestDC
-			$TemplateOID = Get-ADObject $Parameter -Properties msPKI-Cert-Template-OID | findstr "msPKI-Cert-Template-OID" | %{ $_.Split(':')[1]; } | %{$_.Substring(1)} 
+			$TemplateOID = Get-ADObject $Parameter -Properties msPKI-Cert-Template-OID | findstr "msPKI-Cert-Template-OID" | %{ $_.Split(':')[1]; } | %{$_.Substring(1)}
 			write-output "$($TemplateName)=$($TemplateOID)" >> TemplateOID.csv
 		}
-		
+
 		#-------------------------------------------------------------------------------------------------------------------------------------------------------
 		#Find out which Certificate Template is used in the CSR file and which CA Server has the Template and generate the CA Server List menu
 		if ( $CSRfileOID ){
@@ -91,7 +91,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 			$CSRTemplateName = Select-String -Path .\TemplateOID.csv -Pattern $CSRfileOID | foreach{ $_.ToString() } | %{ $_.Split(':')[3]; } | %{ $_.Split('=')[0]; } | select-object -First 1
 			if ( ([string]::IsNullOrEmpty($CSRTemplateName)) ){
 				write-host "This Certificate Template doesn't exist in the CA Servers! Need to re-generate the CSR file or check the Domain Forest!"
-				exit 
+				exit
 			}else {
 				Write-Output "CSR file $($CSRfile) uses Certificate Template:"
 				Write-output $CSRTemplateOID
@@ -109,9 +109,9 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 					}
 				}
 			}
-		}else { 
+		}else {
 			write-output "------------------------------------------------------------------------------------------------------"
-			Write-Output "CSR file $($CSRfile) doesn't use the Certificate Template. Please choose one Template from the following list:" 
+			Write-Output "CSR file $($CSRfile) doesn't use the Certificate Template. Please choose one Template from the following list:"
 			Clear-Content .\TemplateMenu.csv
 			get-content -Path .\TemplateOID.csv | %{ $_.Split('=')[0]; } | Sort-Object | Get-Unique > TemplateMenu.csv
 			$i = 1
@@ -127,13 +127,13 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 				write-output "------------------------------------------------------------------------------------------------------"
 				Write-Output "Selection Wrong, Please correct it and try again."
 			}else { $CSRTemplateName = $selection }
-						
+
 			write-output "------------------------------------------------------------------------------------------------------"
 			write-output "Following CA Servers have this Certificate Template:"
 			$i = 1
 			$menu = @{}
 			foreach($line in Get-Content .\CAServers.csv) {
-				$result = certutil.exe -config $line -CATemplates | findstr $CSRTemplateName":"
+				$result = certutil.exe -config $line -CATemplates | findstr ^$CSRTemplateName":"
 				if ( $result ){
 					Write-Host "$i. $line"
 					$menu.Add($i, ($line))
@@ -141,7 +141,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 				}
 			}
 		}
-		
+
 		# ------------------------------------------------------------------------------------------------------------------------------------------------------
 		#Generate the Certificate.
 		if ( $ENV -eq "-cert" ){
