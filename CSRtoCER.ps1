a617da92c1d6ba05b87b453d966ca2dd58c93342
banhao <banhao@gmail.com>
2019-06-17 11:15:38 -0600

    Update CSRtoCER.ps1

--- CSRtoCER.ps1
+++ CSRtoCER.ps1
@@ -17,23 +17,22 @@
 .NOTES
   Before start running the script, need to install module PSPKI.
   https://www.powershellgallery.com/packages/PSPKI/3.4.1.0
-
+  
   Install-Module -Name PSPKI
-
-  Version:        1.10
+  
+  Version:        1.11
   Author:         <HAO BAN/hao.ban@ehealthsask.ca>
-  Creation Date:  <06/05/2019>
-  Purpose/Change: Fix the bug that can't get CN name when it's a wildcard certificate and the error in the certificate file name.
-				  Use PSPKI to automaticlly issue the certificate which template is configured to issue manually.
-
+  Creation Date:  <06/17/2019>
+  Purpose/Change: Fix the bug that running in Windows 10 that CertUtil version is 10.0.16299.15
+  
 .EXAMPLE
   This PowerShell passed the test in PowerShell version 5.1
-  PS H:\>host
+  PS H:\>host  
 	Check the PowerShell version.
-
+  
   PS H:\>CSRtoCER.ps1 csrfilename
-	Output the Certificate Template and the other information which is in the CSR file.
-
+	Output the Certificate Template and the other information which is in the CSR file. 
+	
   PS H:\>CSRtoCER.ps1 csrfilename -cert
 	Generate the Certificate by using the CSR file.
 
@@ -47,7 +46,7 @@ $ENV = $Args[1]
 
 Import-Module PSPKI
 
-if ( Test-Path -Path TemplatePropCommonName.csv ) { Clear-Content TemplatePropCommonName.csv }else{ New-Item -Name TemplatePropCommonName.csv -ItemType File }
+if ( Test-Path -Path TemplatePropCommonName.csv ) { Clear-Content TemplatePropCommonName.csv }else{ New-Item -Name TemplatePropCommonName.csv -ItemType File } 
 if ( Test-Path -Path TemplateOID.csv ) { Clear-Content TemplateOID.csv }else{ New-Item -Name TemplateOID.csv -ItemType File }
 if ( Test-Path -Path TemplateMenu.csv) { Clear-Content TemplateMenu.csv }else{ New-Item -Name TemplateMenu.csv -ItemType File }
 if ( Test-Path -Path CAServers.csv ) { Clear-Content CAServers.csv }else{ New-Item -Name CAServers.csv -ItemType File }
@@ -66,18 +65,21 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 		$ForestLength = (((get-addomain | findstr "^Forest" |  foreach{ $_.ToString() } | %{ $_.Split(':')[1]; }).Trim()).Split('.')).count
 		$Delta = $DistinguishedNameLength-$ForestLength
 		$ForestDC = ((get-addomain | findstr "DistinguishedName" |  foreach{ $_.ToString() } | %{ $_.Split(':')[1]; }).Trim()).Split(',')[$Delta..$($DistinguishedNameLength-1)] -join ","
-
+		$CertutilVersion = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("c:\windows\system32\certutil.exe").FileVersion | %{ $_.Split('(')[0]; } | foreach{ $_.ToString().Trim() }
+		
 		Write-Output "CSR file $CSRfile Subject = "$CSRfileSubject
 		Write-Output ""
 		Write-Output "CSR file $CSRfile Key Length = $($CSRfileKeyLength)"
 		Write-Output ""
 		Write-Output "CSR file $CSRfile SANs = "$CSRfileSAN
 		Write-Output ""
-
+		
 		#-------------------------------------------------------------------------------------------------------------------------------------------------------
 		#Generate all the Certificate Templates Name List from every CA Server export into TemplatePropCommonName.csv.
 		#certutil.exe -Template | findstr "TemplatePropCommonName" | %{ $_.Split('=')[1]; } |  %{$_.Substring(1)} > .\TemplatePropCommonName.csv
-		certutil.exe | findstr "Config" | %{ $_.Split('`')[1]; } |  %{$_.Substring(0, $_.length - 1) } > CAServers.csv
+		if ( $CertutilVersion -eq "10.0.16299.15" ){ certutil.exe | findstr "Config" | %{ $_.Split(':')[1]; } | foreach{ $_.ToString().Trim() } > CAServers.csv }
+		else{ certutil.exe | findstr "Config" | %{ $_.Split('`')[1]; } |  %{$_.Substring(0, $_.length - 1) } > CAServers.csv }
+		
 		foreach($line in Get-Content .\CAServers.csv) {
 			$CA = certutil.exe -config $line -CATemplates | findstr "Auto-Enroll" | %{ $_.Split(':')[0]; }
 			$COUNT = $CA.count
@@ -91,10 +93,10 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 		foreach($line in Get-Content .\TemplatePropCommonName.csv) {
 			$TemplateName = $line | %{ $_.Split(',')[1]; }
 			$Parameter = "CN="+$TemplateName+",CN=Certificate Templates,CN=Public Key Services,CN=Services,CN=Configuration,"+$ForestDC
-			$TemplateOID = Get-ADObject $Parameter -Properties msPKI-Cert-Template-OID | findstr "msPKI-Cert-Template-OID" | %{ $_.Split(':')[1]; } | %{$_.Substring(1)}
+			$TemplateOID = Get-ADObject $Parameter -Properties msPKI-Cert-Template-OID | findstr "msPKI-Cert-Template-OID" | %{ $_.Split(':')[1]; } | %{$_.Substring(1)} 
 			write-output "$($TemplateName)=$($TemplateOID)" >> TemplateOID.csv
 		}
-
+		
 		#-------------------------------------------------------------------------------------------------------------------------------------------------------
 		#Find out which Certificate Template is used in the CSR file and which CA Server has the Template and generate the CA Server List menu
 		if ( $CSRfileOID ){
@@ -102,7 +104,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 			$CSRTemplateName = Select-String -Path .\TemplateOID.csv -Pattern $CSRfileOID | foreach{ $_.ToString() } | %{ $_.Split(':')[3]; } | %{ $_.Split('=')[0]; } | select-object -First 1
 			if ( ([string]::IsNullOrEmpty($CSRTemplateName)) ){
 				write-host "This Certificate Template doesn't exist in the CA Servers! Need to re-generate the CSR file or check the Domain Forest!"
-				exit
+				exit 
 			}else {
 				Write-Output "CSR file $($CSRfile) uses Certificate Template:"
 				Write-output $CSRTemplateOID
@@ -120,9 +122,9 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 					}
 				}
 			}
-		}else {
+		}else { 
 			write-output "------------------------------------------------------------------------------------------------------"
-			Write-Output "CSR file $($CSRfile) doesn't use the Certificate Template. Please choose one Template from the following list:"
+			Write-Output "CSR file $($CSRfile) doesn't use the Certificate Template. Please choose one Template from the following list:" 
 			get-content -Path .\TemplateOID.csv | %{ $_.Split('=')[0]; } | Sort-Object | Get-Unique > TemplateMenu.csv
 			$i = 1
 			$menu = @{}
@@ -137,7 +139,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 				write-output "------------------------------------------------------------------------------------------------------"
 				Write-Output "Selection Wrong, Please correct it and try again."
 			}else { $CSRTemplateName = $selection }
-
+						
 			write-output "------------------------------------------------------------------------------------------------------"
 			write-output "Following CA Servers have this Certificate Template:"
 			$i = 1
@@ -151,7 +153,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 				}
 			}
 		}
-
+		
 		# ------------------------------------------------------------------------------------------------------------------------------------------------------
 		#Generate the Certificate.
 		if ( $ENV -eq "-cert" ){
@@ -168,7 +170,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 					$RequestId = $RequestIdOutPut[0] | %{ $_.Split(':')[1]; } | foreach{ $_.ToString().Trim() }
 					$CAServer = $selection | %{ $_.Split('\')[0]; } | foreach{ $_.ToString().Trim() }
 					write-output "This Request Id is $($RequestId)"
-					if ( Test-Path -Path "$($CERFILENAME).cer" ) { write-Output "Certificate $($CERFILENAME).cer generate successfully!" }else{ Get-PendingRequest -CA $CAServer -RequestID $RequestId | Approve-CertificateRequest
+					if ( Test-Path -Path "$($CERFILENAME).cer" ) { write-Output "Certificate $($CERFILENAME).cer generate successfully!" }else{ Get-PendingRequest -CA $CAServer -RequestID $RequestId | Approve-CertificateRequest 
 					$CertPath = Get-Location
 					Get-IssuedRequest -CA $CAServer -RequestId $RequestId | Receive-Certificate -Path "$CertPath"
 					Rename-Item -Path "RequestID_$RequestId.cer" -NewName "$CERFILENAME.cer"
@@ -178,7 +180,7 @@ if ( ([string]::IsNullOrEmpty($CSRfile))  ){
 					   $RequestId = $RequestIdOutPut[0] | %{ $_.Split(':')[1]; } | foreach{ $_.ToString().Trim() }
 					   $CAServer = $selection | %{ $_.Split('\')[0]; } | foreach{ $_.ToString().Trim() }
 					   write-output "This Request Id is $($RequestId)"
-					   if ( Test-Path -Path "$($CERFILENAME).cer" ) { write-Output "Certificate $($CERFILENAME).cer generate successfully!" }else{ Get-PendingRequest -CA $CAServer -RequestID $RequestId | Approve-CertificateRequest
+					   if ( Test-Path -Path "$($CERFILENAME).cer" ) { write-Output "Certificate $($CERFILENAME).cer generate successfully!" }else{ Get-PendingRequest -CA $CAServer -RequestID $RequestId | Approve-CertificateRequest 
 					   $CertPath = Get-Location
 					   Get-IssuedRequest -CA $CAServer -RequestId $RequestId | Receive-Certificate -Path "$CertPath"
 					   Rename-Item -Path "RequestID_$RequestId.cer" -NewName "$CERFILENAME.cer"
